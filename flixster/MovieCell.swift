81fb5e2914990302f4f132bcc7179ab67b833120
Sam Gustafsson <samgus93@gmail.com>
2021-02-10 22:01:27 -0500

    Flixster files to Github

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Sam Gustafsson on 2/9/21.
+//  Copyright © 2021 Sam Gustafsson. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

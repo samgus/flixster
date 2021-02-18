db6cc713672e66e82d61d125e7998a4f40725795
Sam Gustafsson <samgus93@gmail.com>
2021-02-18 15:14:05 -0500

    Updated Trailer

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -18,10 +18,12 @@ class MovieDetailsViewController: UIViewController, UICollectionViewDelegate, WK
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var trailerView: WKWebView!
     
     var webView: WKWebView!
-    
     var movie: [String:Any]!
+    var ytBaseUrl = "https://www.youtube.com/watch?v="
+    var trailerId = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +52,37 @@ class MovieDetailsViewController: UIViewController, UICollectionViewDelegate, WK
             
         backDropView.af_setImage(withURL: backDropUrl!)
         
+        getTrailerId()
+    }
+    
+    
+    func getTrailerId(){
+        let baseTrailerUrl = "https://api.themoviedb.org/3/movie/"
+        let movieId = String(movie["id"] as! Int)
+        let restOfUrl = "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"
+        let wholeMovie = URL(string: baseTrailerUrl + movieId + restOfUrl)!
+        let request = URLRequest(url: wholeMovie, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                let results = dataDictionary["results"] as! [[String: Any]]
+                let tempTrailerId = results.first?["key"] as! String
+                self.trailerId = tempTrailerId
+            }
+        }
+        task.resume()
+    }
+
+    @IBAction func tap(_ sender: Any) {
+        let ytWholeUrl = ytBaseUrl + trailerId
+        let request = URLRequest(url: URL(string: ytWholeUrl)!)
         
+        trailerView.load(request)
     }
     
 
